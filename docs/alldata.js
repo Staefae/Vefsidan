6e7707df3963c57a32a56bd5d5096a400d13d127
OliverMani <oliverhilmars@gmail.com>
2020-12-02 12:56:10 +0000

    Blandaði alldata#

--- docs/alldata.js
+++ docs/alldata.js
@@ -1,1 +1,1 @@
-const LIST = [{'nafn':'Hliðrun falla', 'skra':'hlidrun'},{'nafn':'Einföld stærfræði', 'skra':'simple'}]
+const LIST = [{'nafn':'Hliðrun falla', 'skra':'hlidrun'},{'nafn':'Einföld stærðfræði', 'skra':'simple'},{'nafn':'Jöfnur', 'skra':'jofnur'}]
